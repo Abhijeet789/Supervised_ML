c7e1045a8a347ccec17d5458d155cf78a0e6676e
Abhijeet Aher <aher.abhijeet789@gmail.com>
2018-06-19 14:49:26 +0530

    Update and rename project3.py to 2D_array_operations.py

--- 2D_array_operations.py
+++ 2D_array_operations.py
@@ -1,23 +1,29 @@
+#this is the program to perform some operaations on array
+
+
+#importing libraries
 import numpy 
 import os
 
+#taking the input for matrix-1 from user
 x=int(input("\nEnter the number of rows of matrix1"))
 y=int(input("\nEnter the number of coloumns of matrix1"))
 val = int(input("\nEnter the value of element of matrix1"))
 
-
+#taking the input for matrix-2 from user
 x1=int(input("\nEnter the number of rows of matrix2"))
 y1=int(input("\nEnter the number of coloumns of matrix2"))
 val1 = int(input("\nEnter the value of element of matrix2"))
 
 
-
+#creating the array 
 array1 = numpy.full((x,y),val)
 array2 = numpy.full((x1,y1),val1)
 
 print(array1)
 print(array2)
 
+#displaying the menu
 X = '''
 	press 1 for addition of two matrix
 	press 2 for substraction of two matrix
@@ -25,6 +31,8 @@ X = '''
 	press 4 for division of two matrix
 '''
 print (X)
+
+#taking the input from the user to perform perticular operation implemented by if-else
 choice = int(input("enter your choice"))
 
 if choice == 1:
@@ -54,9 +62,10 @@ elif choice == 4:
 else :
 	print("\nYou have entered a wrong input")
 
+#taking the input of staoring location from user
 loc = input("\nPlease enter the location where you wants to save the outputs ")
 
-
+#saving the output array on user define location
 os.system("touch /home/keshav/"+loc+"/k13.txt | mv k11.txt /home/keshav/"+loc+"/k13.txt")
 
  
