102fda52978316161b65623757f5b7c5211dd004
Abhijeet Aher <aher.abhijeet789@gmail.com>
2018-06-19 14:43:20 +0530

    Update and rename project2.py to 2D_array.py

--- 2D_array.py
+++ 2D_array.py
@@ -1,6 +1,10 @@
+#this program is to implement use of matrix using numpy
+
+#importing python libraries
 import numpy
 import os
 
+#taking the inputs from the user
 x=int(input("\nEnter the number of rows of matrix"))
 
 y=int(input("\nEnter the number of coloumns of matrix"))
@@ -13,6 +17,7 @@ array1 = numpy.full((x,y),val)
 
 numpy.savetxt('k11.txt', array1)
 
+#printing the matrix
 print(array1)
 
 os.system("touch /home/keshav/"+loc+"/k12.txt | mv k11.txt /home/keshav/"+loc+"/k12.txt")
