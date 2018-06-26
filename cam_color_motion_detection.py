6613cc2b918ea7d3d0ba69811b98790d40717b99
Abhijeet Aher <aher.abhijeet789@gmail.com>
2018-06-26 23:32:20 +0530

    Add files via upload
    This is a python3 program which is used to detect the motion of red color in front of the camera using absolutedifference(absdiff) function and displays it on the screen using opencv.

---
+++ cam_color_motion_detection.py
@@ -0,0 +1,47 @@
+#This is a python3 program which is to detect the motion infront of the webcam
+
+#importing the libraries
+import cv2
+
+#declaring the function to process on gthe frame and to calculate the difference in frames
+def img_dif(x,y,z):
+	img1 = cv2.absdiff(x,y)
+	img2 = cv2.absdiff(y,z)
+	dif = cv2.bitwise_and(img1,img2)
+	return dif
+
+#opening the camera
+capture = cv2.VideoCapture(0)
+
+#taking the three continuous frames 
+frame1 = capture.read()[1]
+frame2 = capture.read()[1]
+frame3 = capture.read()[1]
+
+#converting the captured frames into black and white format
+gray1 = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+gray3 = cv2.cvtColor(frame3,cv2.COLOR_BGR2GRAY)
+
+#taking the loop to detect the motion in the frames
+while capture.isOpened():
+	#passing the captured frames as the argument of the function img_dif	
+	img_motion = img_dif(gray1,gray2,gray3)
+	#displaying the motion 
+	cv2.imshow("motion",img_motion)
+	#capturing the new frame
+	status , frame = capture.read()
+	#detecting only red part of the image
+	only_red = cv2.inRange(frame,(0,0,100),(40,40,255))
+	#to detect the motion, comparing three new frames
+	gray1 = gray2
+	gray2 = gray3
+	gray3 = only_red
+	#holding the frame and exiting from the program when q is pressed
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+#releasing the camera
+capture.release()
+#closing all the windows
+cv2.destroyAllWindows()
